e94eb6829f16024d57566ce28fa827af679d6e01
Meriemcharef <148784772+Meriemcharef@users.noreply.github.com>
2023-12-15 14:06:46 -0800

    Add files via upload
    DES PAGES

---
+++ Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loginn from '../components/LoginForm';
+ 
+ function Login() {
+   return (
+     <div>
+        <Loginn/>
+     </div>
+   )
+ }
+ 
+ export default Login;
+ 
